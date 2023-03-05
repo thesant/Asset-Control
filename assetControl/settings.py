f8ce49e312c6daf9f86090c95dbd887a822cb613
Samuel Santana <samuelsantana95@outlook.com>
2023-03-05 19:11:08 -0300

    remove comment

--- assetControl/settings.py
+++ assetControl/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-!m&cx8hk7o4de!99##jx)@_o*g=)9=@2bjl#vs)6u*5d=l7l24'
+
 SECRET_KEY = os.environ.get('SECRET_KEY', 'INSEGURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
